8a78b99b3630e3f815537e06d71953de617d6c4e
EC2 Default User <ec2-user@ip-172-31-89-176.ec2.internal>
2019-11-18 16:44:56 +0000

    initial files and repo creation

---
+++ liri.js
@@ -0,0 +1,10 @@
+//require & import  modules
+let axios = require('axios');
+let spotify = require('node-spotify-api');
+let moment = require('moment');
+let dotenv = require('dotenv');
+
+require("dotenv").config();
+
+
+let spotifyKey= new Spotify(keys.spotify);
